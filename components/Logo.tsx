20e80a7fc25a02d5b4a95584061dcb80eccaf4c6
mhmdbhsk <muhammadbhaska0@gmail.com>
2022-12-04 02:52:56 +0700

    feat: initial commit

---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+import { IconDrone } from "@tabler/icons";
+
+const Logo = () => {
+  return (
+    <div className="flex gap-2">
+      <IconDrone />
+      <span className="font-bold">Jentayu VTOL</span>
+    </div>
+  );
+};
+
+export default Logo;
